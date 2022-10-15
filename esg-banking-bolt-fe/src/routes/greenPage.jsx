936ec0a7c8f178cc49e4bac3352624d911cc3668
christrobinong <e0407801@u.nus.edu>
2022-10-15 23:20:56 +0800

    green page: list of rewards, promotion, and nfts

--- esg-banking-bolt-fe/src/routes/greenPage.jsx
+++ esg-banking-bolt-fe/src/routes/greenPage.jsx
@@ -8,15 +8,20 @@ import ButtonBackground from '../assets/green-button-background.svg';
 import {ReactComponent as BackButton} from '../assets/back-button.svg';
 import { ReactComponent as ArrowRight } from '../assets/return-up-forward-outline.svg';
 import { ReactComponent as Leaf } from '../assets/leaf.svg';
+import { ReactComponent as GreenLeaf } from '../assets/green-leaf.svg';
 import CarbonLogo from '../assets/carbon-footprint.png';
 import { ReactComponent as GreenRightArrow } from '../assets/rightarrow-green.svg';
-
+import ChickenKFC from '../assets/kfc-chicken.png';
+import KFCLogo from '../assets/kfc-logo.png';
+import Promo1 from '../assets/promo1.png';
+import Promo2 from '../assets/promo2.png';
+import NFT1 from '../assets/nft1.png';
 
 const GreenPage = styled.div`
   box-sizing: border-box;
   background-color: #F8F8F8;
   width: 100%;
-  height: 100vh;
+  height: auto;
   display: flex;
   flex-direction: column;
 `
@@ -95,6 +100,44 @@ const CarbonButton = styled(Button)`
   background-size:100% 100%;
   display: flex !important;
   justify-content: space-between !important;
+`
+
+const Subtitle = styled.p`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 10px;
+  line-height: 12px;
+  color: #828282;
+`
+
+const Title = styled.p`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 17px;
+  color: #0D0D0D;
+  margin-block-start: 0;
+  margin-block-end: 0;
+`
+
+const Cards = styled.div`
+  display: flex;
+  overflow-x: auto;
+  padding: 0px 24px;
+  gap: 8px;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+const Card = styled.div`
+  border-radius: 14px;
+  background-color: white;
 `
 
 function greenPage() {
@@ -212,6 +255,147 @@ function greenPage() {
           </div>  
         </RowFlex>
       </CarbonButton>
+
+      <ColumnFlex>
+        <RowFlex style={{ padding: "12px 26px", marginTop:'25px'}}>
+          <Title>Rewards</Title>
+          <Subtitle>View All</Subtitle>
+        </RowFlex>
+
+        <Cards>
+          <Card> 
+            <ColumnFlex>
+              <img src={ChickenKFC} alt="" height={72} />
+              <RowFlex style={{marginLeft: 10, marginRight: 10}}>
+                <p style={{fontSize: 10, fontWeight: 'bold'}} >20% Fried Chicken at KFC</p>
+                <img src={KFCLogo} alt="" width={24} height={24} style={{marginTop: 10}} />
+              </RowFlex>
+              <Subtitle style={{marginLeft: 10, marginRight:10, marginTop:-3}}>Valid until: 31 Sept 22</Subtitle>
+              <RowFlex style={{marginLeft: 10, marginRight: 10, marginTop:0, marginBottom: 10}}>
+                <div>
+                  <GreenLeaf/>
+                  <span style={{fontSize: 12, fontWeight: 'bold', marginLeft:5, color: '#5DB06C'}}> 50 </span>
+                </div>
+                <div>
+                  <span style={{fontSize: 12, textDecoration:'underline', color:'#5DB06C'}}>Redeem</span>
+                </div>
+              </RowFlex>
+            </ColumnFlex>
+          </Card>
+          <Card> 
+            <ColumnFlex>
+              <img src={ChickenKFC} alt="" height={72} />
+              <RowFlex style={{marginLeft: 10, marginRight: 10}}>
+                <p style={{fontSize: 10, fontWeight: 'bold'}} >20% Fried Chicken at KFC</p>
+                <img src={KFCLogo} alt="" width={24} height={24} style={{marginTop: 10}} />
+              </RowFlex>
+              <Subtitle style={{marginLeft: 10, marginRight:10, marginTop:-3}}>Valid until: 31 Sept 22</Subtitle>
+              <RowFlex style={{marginLeft: 10, marginRight: 10, marginTop:0, marginBottom: 10}}>
+                <div>
+                  <GreenLeaf/>
+                  <span style={{fontSize: 12, fontWeight: 'bold', marginLeft:5, color: '#5DB06C'}}> 50 </span>
+                </div>
+                <div>
+                  <span style={{fontSize: 12, textDecoration:'underline', color:'#5DB06C'}}>Redeem</span>
+                </div>
+              </RowFlex>
+            </ColumnFlex>
+          </Card>
+          <Card> 
+            <ColumnFlex>
+              <img src={ChickenKFC} alt="" height={72} />
+              <RowFlex style={{marginLeft: 10, marginRight: 10}}>
+                <p style={{fontSize: 10, fontWeight: 'bold'}} >20% Fried Chicken at KFC</p>
+                <img src={KFCLogo} alt="" width={24} height={24} style={{marginTop: 10}} />
+              </RowFlex>
+              <Subtitle style={{marginLeft: 10, marginRight:10, marginTop:-3}}>Valid until: 31 Sept 22</Subtitle>
+              <RowFlex style={{marginLeft: 10, marginRight: 10, marginTop:0, marginBottom: 10}}>
+                <div>
+                  <GreenLeaf/>
+                  <span style={{fontSize: 12, fontWeight: 'bold', marginLeft:5, color: '#5DB06C'}}> 50 </span>
+                </div>
+                <div>
+                  <span style={{fontSize: 12, textDecoration:'underline', color:'#5DB06C'}}>Redeem</span>
+                </div>
+              </RowFlex>
+            </ColumnFlex>
+          </Card>
+        </Cards>
+      </ColumnFlex>
+
+      <ColumnFlex>
+        <RowFlex style={{ padding: "12px 24px", marginTop:'25px'}}>
+          <Title>Promotions</Title>
+          <Subtitle>View All</Subtitle>
+        </RowFlex>
+        <Cards>
+          <img src={Promo1} alt="" width={250} height={150}/>
+          <img src={Promo2} alt="" width={250} height={150}/>
+        </Cards>
+      </ColumnFlex>
+
+      <ColumnFlex style={{marginBottom: 20}}>
+        <RowFlex style={{ padding: "12px 26px", marginTop:'25px'}}>
+          <Title>NFTs</Title>
+          <Subtitle>View All</Subtitle>
+        </RowFlex>
+        <Cards>
+          <Card> 
+              <ColumnFlex>
+                <img src={NFT1} alt="" height={100} />
+                <RowFlex style={{marginLeft: 10, marginRight: 10}}>
+                  <p style={{fontSize: 10, fontWeight: 'bold'}} >Green APE BAYC Collection 2022</p>
+                </RowFlex>
+                <Subtitle style={{marginLeft: 10, marginRight:10, marginTop:-3}}>By: Rider Clips</Subtitle>
+                <RowFlex style={{marginLeft: 10, marginRight: 10, marginTop:0, marginBottom: 10}}>
+                  <div>
+                    <GreenLeaf/>
+                    <span style={{fontSize: 12, fontWeight: 'bold', marginLeft:5, color: '#5DB06C'}}> 50 </span>
+                  </div>
+                  <div>
+                    <span style={{fontSize: 12, textDecoration:'underline', color:'#5DB06C'}}>More Info</span>
+                  </div>
+                </RowFlex>
+              </ColumnFlex>
+          </Card>
+          <Card> 
+              <ColumnFlex>
+                <img src={NFT1} alt="" height={100} />
+                <RowFlex style={{marginLeft: 10, marginRight: 10}}>
+                  <p style={{fontSize: 10, fontWeight: 'bold'}} >Green APE BAYC Collection 2022</p>
+                </RowFlex>
+                <Subtitle style={{marginLeft: 10, marginRight:10, marginTop:-3}}>By: Rider Clips</Subtitle>
+                <RowFlex style={{marginLeft: 10, marginRight: 10, marginTop:0, marginBottom: 10}}>
+                  <div>
+                    <GreenLeaf/>
+                    <span style={{fontSize: 12, fontWeight: 'bold', marginLeft:5, color: '#5DB06C'}}> 50 </span>
+                  </div>
+                  <div>
+                    <span style={{fontSize: 12, textDecoration:'underline', color:'#5DB06C'}}>More Info</span>
+                  </div>
+                </RowFlex>
+              </ColumnFlex>
+          </Card>
+          <Card> 
+              <ColumnFlex>
+                <img src={NFT1} alt="" height={100} />
+                <RowFlex style={{marginLeft: 10, marginRight: 10}}>
+                  <p style={{fontSize: 10, fontWeight: 'bold'}} >Green APE BAYC Collection 2022</p>
+                </RowFlex>
+                <Subtitle style={{marginLeft: 10, marginRight:10, marginTop:-3}}>By: Rider Clips</Subtitle>
+                <RowFlex style={{marginLeft: 10, marginRight: 10, marginTop:0, marginBottom: 10}}>
+                  <div>
+                    <GreenLeaf/>
+                    <span style={{fontSize: 12, fontWeight: 'bold', marginLeft:5, color: '#5DB06C'}}> 50 </span>
+                  </div>
+                  <div>
+                    <span style={{fontSize: 12, textDecoration:'underline', color:'#5DB06C'}}>More Info</span>
+                  </div>
+                </RowFlex>
+              </ColumnFlex>
+          </Card>
+        </Cards>
+      </ColumnFlex>
     </GreenPage>
   );
 }
