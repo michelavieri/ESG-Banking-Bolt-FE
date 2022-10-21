5de97fe204c384edf5c7bc96cd2df38585cf77bb
christrobinong <e0407801@u.nus.edu>
2022-10-21 10:09:04 +0800

    list of rewards page (#4)

--- esg-banking-bolt-fe/src/index.jsx
+++ esg-banking-bolt-fe/src/index.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import GreenPage from './routes/greenPage';
 import TreePage from './routes/treePage';
+import ListRewardsPage from './routes/listRewardsPage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +15,7 @@ root.render(
         <Route path="/" element={<App />} />
         <Route path="/green" element={<GreenPage />} />
         <Route path="/tree" element={<TreePage />} />
+        <Route path="list-rewards" element={<ListRewardsPage/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
